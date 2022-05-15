304f2941912f2e78882268fab4bcf0ac82bc25fe
ablil <ablil@protonmail.com>
2022-05-15 22:58:05 +0100

    update landing page ui

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,48 @@ import Image from "next/image";
 import Link from "next/link";
 import CustomFooter from "../components/common/CustomFooter";
 import CustomNavbar from "../components/common/CustomNavbar";
+import Computer from "../components/icons/landingpage/Computer";
 // import styles from "../styles/Home.module.css";
 import showcase from "../public/images/showcase.png";
 
 export default function Home() {
   return (
     <>
-      <CustomNavbar />
       <main>
         <section id="main-banner">
+          <CustomNavbar />
           <header>The perfect place for developers</header>
           <article>
             you can save your side projects ideas that comes to you mind, an
             keep track of your progress, features, tech stack ...
           </article>
+          <Computer />
           <footer>
             <Link href="/register">register now</Link>
           </footer>
         </section>
 
-        <section id="showcase">
-          <img src="/images/showcase.png" alt="showcase" />
+        <section className="showcase">
+          <p>Save all your ideas in one place with quick and easy access</p>
+          <img src="/images/showcase/dashboard.png" alt="dashboard" />
+        </section>
+        <section className="showcase">
+          <p>
+            Create a backlog of task you want to do and what status each task is
+            in
+          </p>
+          <img src="/images/showcase/backlog.png" alt="backlog" />
+        </section>
+        <section className="showcase">
+          <p>
+            Have access to reminders section that act as a quick todo list for
+            thing unrelated to any projec
+          </p>
+          <img src="/images/showcase/reminders.png" alt="reminders" />
+        </section>
+        <section className="showcase">
+          <p>save technical metadata</p>
+          <img src="/images/showcase/metadata.png" alt="metadata" />
         </section>
 
         <section id="features">
