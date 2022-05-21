be9031ac6fdcd0f3f593965bebffe4a2ce4e4507
ablil <ablil@protonmail.com>
2022-05-21 15:26:29 +0100

    fix: hide login/register button when user is authenticated

--- components/common/CustomNavbar.js
+++ components/common/CustomNavbar.js
@@ -1,14 +1,23 @@
 import Link from "next/link";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import metadata from "../../metadata";
+import { auth } from "../../services/firebaseconfig";
 
 const CustomNavbar = () => {
+  const [user, loading, error] = useAuthState(auth);
+
   return (
     <nav id="landingpage-navbar">
       <h1>{metadata.brand}</h1>
       <section>
-        <Link href="/login">login</Link>
-        <Link href="/register">register</Link>
+        {!user && !loading && (
+          <>
+            <Link href="/login">login</Link>
+            <Link href="/register">register</Link>
+          </>
+        )}
+        {user && !loading && <Link href="/ideas">App</Link>}
       </section>
     </nav>
   );
